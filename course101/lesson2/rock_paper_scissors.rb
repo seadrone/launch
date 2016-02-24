1d249c2592b3aaf37570a35be6901da5105a1e72
Grace D <gsdrone@gmail.com>
2016-02-24 03:49:59 +0000

    Basic RPS game working

---
+++ course101/lesson2/rock_paper_scissors.rb
@@ -0,0 +1,43 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_result(player, computer)
+  if (player == 'rock' && computer == 'scissors') || 
+      (player == 'scissors' && computer == 'paper') || 
+      (player == 'paper' && computer == 'rock')
+    prompt("You win!")
+  elsif (computer == 'rock' && player == 'scissors') || 
+      (computer == 'scissors' && player == 'paper') || 
+      (computer == 'paper' && player == 'rock')
+    prompt("Computer wins!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do 
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+    
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+  
+  computer_choice = VALID_CHOICES.sample()
+  puts "You chose: #{choice}.\nComputer chose: #{computer_choice}."
+  
+  display_result(choice, computer_choice)
+  
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thanks for playing. Goodbye!")
