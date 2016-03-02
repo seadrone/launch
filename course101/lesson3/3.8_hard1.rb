a95f0ba4bda1e210be9ce841d72b667a3d97cca6
Grace D <gsdrone@gmail.com>
2016-03-02 18:49:57 +0000

    finish 3.8

--- course101/lesson3/3.8_hard1.rb
+++ course101/lesson3/3.8_hard1.rb
@@ -82,4 +82,15 @@ def create_UUID()
   uuid.chop
 end
 
-p create_UUID()
+# 5
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.count('.') == 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_a_number?(word)
+  end
+  return true
+end
+
