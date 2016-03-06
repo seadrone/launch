bc461ef8e02349e238b2a6c2497f30e4fb1d8a24
Grace D <gsdrone@gmail.com>
2016-03-06 03:35:10 +0000

    scoring is working

--- course101/lesson4/ttt_bonus.rb
+++ course101/lesson4/ttt_bonus.rb
@@ -4,18 +4,26 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 5, 9]]
+NUM_TO_WIN = 2
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+def joinor(arr, punct, word)
+  return arr.join() if arr.size < 2
+  last_val = arr.pop
+  arr.join("#{punct} ")<< "#{punct} #{word} #{last_val}"
+end
+
 # rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
-  puts "You're #{PLAYER_MARKER} and the computer is #{COMPUTER_MARKER}."
+  puts "You are #{PLAYER_MARKER}'s and the computer is #{COMPUTER_MARKER}'s."
+  puts "The first player to win #{NUM_TO_WIN} games wins."
   puts ""
   puts "     |     |"
-  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
@@ -23,7 +31,7 @@ def display_board(brd)
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
   puts ""
 end
@@ -37,12 +45,6 @@ end
 
 def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
-end
-
-def joinor(arr, punct, word)
-  return arr.join() if arr.size < 2
-  last_val = arr.pop
-  arr.join("#{punct} ")<< "#{punct} #{word} #{last_val}"
 end
 
 def player_places_piece!(brd)
@@ -73,7 +75,7 @@ end
 def detect_winner(brd)
   WINNING_LINES.each do |line|
     if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
-      return 'Player'
+      return 'You'
     elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
@@ -81,10 +83,19 @@ def detect_winner(brd)
   nil
 end
 
+def display_game_winner(brd)
+  prompt("#{detect_winner(brd)} won!") if someone_won?(brd)
+  prompt("It's a tie!") if (board_full?(brd) && !detect_winner(brd))
+end
+
+user_score = 0
+computer_score = 0
+
 loop do
   board = initialize_board
   loop do
     display_board(board)
+    prompt("Your score is: #{user_score}. Computer's score is: #{computer_score}.")
     player_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
     computer_places_piece!(board)
@@ -94,14 +105,30 @@ loop do
 
   display_board(board)
 
-  if someone_won?(board)
-    prompt("#{detect_winner(board)} won!")
-  else
-    prompt("It's a tie!")
+  user_score += 1 if detect_winner(board) == 'You'
+  computer_score += 1 if detect_winner(board) == 'Computer'
+
+  display_game_winner(board)
+  sleep(1.2) unless user_score == NUM_TO_WIN || computer_score == NUM_TO_WIN
+
+  if user_score == NUM_TO_WIN
+    user_score = 0
+    computer_score = 0
+    prompt("Congratulations!")
+    prompt("Do you want to play again? (y or n)")
+    answer = gets.chomp
+    break unless answer.downcase.start_with?('y')
+  elsif computer_score == NUM_TO_WIN
+    user_score = 0
+    computer_score = 0
+    prompt("Bummer, the computer wins.")
+    prompt("Do you want to play again? (y or n)")
+    answer = gets.chomp
+    break unless answer.downcase.start_with?('y')
   end
-  
-  prompt("Do you want to play again? (y or n)")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+
 end
+
 prompt("Thanks for playing. Goodbye.")
+
+
