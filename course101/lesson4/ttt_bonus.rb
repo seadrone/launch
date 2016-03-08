46c5008a05f6dc86415f073878369c1cba2b701f
Grace D <gsdrone@gmail.com>
2016-03-08 04:35:19 +0000

    bonus refactoring and bug fixes

--- course101/lesson4/ttt_bonus.rb
+++ course101/lesson4/ttt_bonus.rb
@@ -4,7 +4,8 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 5, 9]]
-NUM_TO_WIN = 5
+NUM_TO_WIN = 2
+STARTING_PLAYER = "choose"
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -78,11 +79,7 @@ def computer_offense!(brd, square)
 end
 
 def check_square_five?(brd)
-  if brd[5] == ' '
-    return true
-  else
-    return false
-  end
+  brd[5] == ' '
 end
 
 def computer_places_piece!(brd)
@@ -118,30 +115,69 @@ def display_game_winner(brd)
   prompt("It's a tie!") if (board_full?(brd) && !detect_winner(brd))
 end
 
+def computer_turn!(brd)
+  offense_square = detect_third_square(brd, COMPUTER_MARKER) # computer offensive logic
+  if offense_square 
+    computer_offense!(brd, offense_square) 
+  else
+    defense_square = detect_third_square(brd, PLAYER_MARKER) # computer defense logic
+    if defense_square
+      computer_defend!(brd, defense_square)
+    else
+      computer_places_piece!(brd)
+    end
+  end
+end
+
+def place_piece!(brd, player)
+  if player == 1 # 1 is the user
+    player_places_piece!(brd)
+  elsif player == 2 # 2 is the computer
+    computer_turn!(brd)
+  end
+end
+
+def alternate_player(player)
+  next_player = 2 if player == 1
+  next_player = 1 if player == 2
+  return next_player
+end
+
+def choose_first_player
+  prompt("Would you like to go first (y or n)?")
+  user_choice = gets.chomp
+  if user_choice == 'y' 
+    return 1  
+  else 
+    return 2
+  end
+end
+
+def play_again?(winner, user_score, computer_score)
+  prompt("Congratulations!") if winner == 'You'
+  prompt("Bummer, the computer won.") if winner == 'Computer'
+  prompt("The final score is: You: #{user_score} Computer: #{computer_score}")
+  prompt("Do you want to play again (y or n)?")
+  answer = gets.chomp
+  return answer
+end
+
 user_score = 0
 computer_score = 0
+
+if STARTING_PLAYER == 'choose'
+  current_player = choose_first_player 
+else
+  current_player = STARTING_PLAYER
+end
 
 loop do
   board = initialize_board
   loop do
     display_board(board)
     prompt("Your score is: #{user_score}. Computer's score is: #{computer_score}.")
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-    
-    offense_square = detect_third_square(board, COMPUTER_MARKER) # computer offensive logic
-    if offense_square 
-      computer_offense!(board, offense_square) 
-    else
-      defense_square = detect_third_square(board, PLAYER_MARKER) # computer defense logic
-      if defense_square
-        computer_defend!(board, defense_square)
-      else
-        computer_places_piece!(board)
-      end
-    end
-
-    display_board(board)
+    place_piece!(board, current_player)
+    current_player = alternate_player(current_player)
     break if someone_won?(board) || board_full?(board)
   end
 
@@ -154,25 +190,19 @@ loop do
   sleep(1.5) unless user_score == NUM_TO_WIN || computer_score == NUM_TO_WIN
 
   if user_score == NUM_TO_WIN
-    prompt("Congratulations!")
-    prompt("The final score is: You: #{user_score} Computer: #{computer_score}")
-    prompt("Do you want to play again? (y or n)")
-    user_score = 0
-    computer_score = 0
-    answer = gets.chomp
+    answer = play_again?('You', user_score, computer_score)
     break unless answer.downcase.start_with?('y')
   elsif computer_score == NUM_TO_WIN
-    prompt("Bummer, the computer wins.")
-    prompt("The final score is: You: #{user_score} Computer: #{computer_score}")
-    prompt("Do you want to play again? (y or n)")
-    user_score = 0
-    computer_score = 0
-    answer = gets.chomp
+    answer = play_again?('Computer', user_score, computer_score)
     break unless answer.downcase.start_with?('y')
+  end
+
+  if STARTING_PLAYER == 'choose'
+    current_player = choose_first_player 
+  else
+    current_player = STARTING_PLAYER
   end
 
 end
 
 prompt("Thanks for playing. Goodbye.")
-
-
