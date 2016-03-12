9e639c1f0fe70b4b6d96aeffa3431996edace007
Grace D <gsdrone@gmail.com>
2016-03-12 01:20:44 +0000

    bug fixes and refactoring

--- course101/lesson4/twenty-one.rb
+++ course101/lesson4/twenty-one.rb
@@ -23,58 +23,37 @@ def read_card(card)
   "#{card[0]} of #{card[1]}"
 end
 
-def print_hand(hand, who)
+def print_hand(hand, show_or_hide)
   printed_hand = []
   hand.each { |card| printed_hand << read_card(card) }
-  if hand.size == 2 && who == 'player'
+  if hand.size == 2 && show_or_hide == 'show'
     printed_hand.join(' and ')
-  elsif hand.size > 2 && who == 'player'
+  elsif hand.size > 2 && show_or_hide == 'show'
     last_card = printed_hand.pop
     printed_hand.join(', ') << ' and ' << last_card
-  elsif who == 'dealer'
+  elsif hand.size == 2 && show_or_hide == 'hide'
+    printed_hand[0] << ' and ' << '??'
+  elsif hand.size > 2 && show_or_hide == 'hide'
     last_card = printed_hand.pop
-    printed_hand.join(', ') << ' and ' << "hidden card"
-  end
-end
-
-def look_up_values(card)
-  rank = card[0]
-  case rank
-  when '2'
-    2
-  when '3'
-    3
-  when '4'
-    4
-  when '5'
-    5
-  when '6'
-    6
-  when '7'
-    7
-  when '8'
-    8
-  when '9'
-    9
-  when '10'
-    10
-  when 'Jack'
-    10
-  when 'Queen'
-    10
-  when 'King'
-    10
-  when 'Ace'
-    1
+    printed_hand.join(', ') << ' and ' << '??'
   end
 end
 
 def calculate_score(hand)
   score = 0
-  hand.each do |card| 
-    score += look_up_values(card)
+  hand.each do |card|
+    if card[0] == 'A'
+      score += 11
+    elsif card[0].to_i == 0
+      score += 10
+    else
+      score += card[0].to_i
+    end
   end
-  score += 10 if hand.count('Ace') > 1
+  hand.select {|value| value == 'Ace'}.count.times do 
+    score -= 10 if score > 21
+  end
+  
   score
 end
 
@@ -86,13 +65,13 @@ def hit!(deck, hand)
   hand.unshift(deal_card!(deck))
 end
 
-def who_won?(player_score, dealer_score)
-  if player_score > dealer_score && !bust?(player_score)
+def who_won(player_score, dealer_score)
+  if player_score > dealer_score 
     "You won"
-  elsif dealer_score > player_score && !bust?(dealer_score)
+  elsif dealer_score > player_score
     "Dealer wins"
   else 
-    "Push"
+    "It's a tie"
   end
 end
 
@@ -112,42 +91,54 @@ deck = new_deck
 
 dealer_score = calculate_score(dealer_hand)
 player_score = calculate_score(player_hand)
-prompt("Dealer hand: #{print_hand(dealer_hand, 'dealer')}.")
-prompt("Your hand: #{print_hand(player_hand, 'player')}. Your score: #{player_score}")
+prompt("Dealer's hand: #{print_hand(dealer_hand, 'hide')}.")
+prompt("Your hand: #{print_hand(player_hand, 'show')}. Your total: #{player_score}")
 
-answer = nil
 loop do
   prompt("Hit or stay (h or s)?")
   answer = gets.chomp
   if answer == 'h'
-    hit!(deck, player_hand) if answer == 'h'
+    prompt("You chose to hit")
+    hit!(deck, player_hand)
     player_score = calculate_score(player_hand)
-    prompt("Your hand: #{print_hand(player_hand, 'player')}. Your score: #{player_score}")
-  elsif answer != 'h' && answer != 's'
+    prompt("Your hand is now: #{print_hand(player_hand, 'show')}. Your total: #{player_score}")
+    break if bust?(player_score)
+  elsif answer == 's'
+    prompt("You chose to stay at #{player_score}.")
+    prompt("================")
+    break
+  else
     prompt("That's not a valid choice. Please choose 'h' to hit or 's' to stay.")
   end
-  break if answer == 's' || bust?(player_score)
 end
 
 if bust?(player_score)
   prompt("You bust! The dealer wins.")
-end
-
-loop do
-  if dealer_score >= 17 || bust?(dealer_score)
-    break
-  else
-    hit!(deck, dealer_hand)
-    dealer_score = calculate_score(dealer_hand)
+else
+  prompt("The dealer's turn...")
+  loop do
+    if dealer_score >= 17 || bust?(dealer_score)
+      prompt("The dealer stays at #{dealer_score}")
+      break
+    else
+      prompt("The dealer hits")
+      hit!(deck, dealer_hand)
+      dealer_score = calculate_score(dealer_hand)
+      prompt("The dealer's hand is now: #{print_hand(dealer_hand, 'show')}. Dealer's total is #{dealer_score}")
+      prompt("================")
+      break if bust?(dealer_score)
+    end
   end
 end
 
 if bust?(dealer_score)
   prompt("Dealer busts! You win.")
+else
+  prompt("================")
+  prompt(who_won(player_score, dealer_score)) unless bust?(dealer_score) || bust?(player_score)
+  prompt("The dealer had: #{print_hand(dealer_hand, 'show')}. Dealer's total: #{dealer_score}")
+  prompt("You had: #{print_hand(player_hand, 'show')}. Your total: #{player_score}")
+  prompt("================")
 end
 
-prompt(who_won?(player_score, dealer_score)) unless bust?(dealer_score) || bust?(player_score)
-
-prompt("The dealer had: #{print_hand(dealer_hand, 'player')} (#{dealer_score})")
-prompt("You had: #{print_hand(player_hand, 'player')} (#{player_score})")
 prompt("Thanks for playing Twenty-one!")
