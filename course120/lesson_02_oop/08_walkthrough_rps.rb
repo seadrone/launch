bbd225fd5236078c2e01ad4a291a288b0dc3c873
Grace D <gsdrone@gmail.com>
2016-04-15 03:08:22 +0000

    basic rps game

--- course120/lesson_02_oop/08_walkthrough_rps.rb
+++ course120/lesson_02_oop/08_walkthrough_rps.rb
@@ -1,8 +1,24 @@
 class Player
-  attr_accessor :move
+  attr_accessor :move, :name
   def initialize(player_type = :human)
     @player_type = player_type
     @move = nil
+    set_name
+  end
+  
+  def set_name
+    if human?
+      n = ""
+      loop do
+        puts "What's your name?"
+        n = gets.chomp
+        break unless n.empty?
+        puts "Please enter a name."
+      end
+      self.name = n
+    else
+      self.name = ['R2D2', 'Hal', 'Computer'].sample
+    end
   end
   
   def choose
@@ -41,11 +57,47 @@ class RPSGame
     puts "Thanks for playing Rock, Paper, Scissors. Goodbye."
   end
   
+  def display_winner
+    puts "#{human.name} chose #{human.move}"
+    puts "#{computer.name} chose #{computer.move}"
+    
+    case human.move
+    when "rock"
+      puts "It's a tie!" if computer.move == "rock"
+      puts "#{human.name} wins!" if computer.move == "scissors"
+      puts "#{computer.name} wins." if computer.move == "paper"
+    when "paper"
+      puts "It's a tie!" if computer.move == "paper"
+      puts "#{human.name} wins!" if computer.move == "rock"
+      puts "#{computer.name} wins." if computer.move == "scissors"
+    when "scissors"
+      puts "It's a tie!" if computer.move == "scissors"
+      puts "#{human.name} wins!" if computer.move == "paper"
+      puts "#{computer.name} wins." if computer.move == "rock"
+    end
+    
+  end
+  
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include? answer.downcase
+      puts "Sorry you must choose y or n."
+    end
+    return true if answer == 'y'
+    return false
+  end
+  
   def play
     display_welcome_message
-    human.choose
-    computer.choose
-    display_winner
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
 end
