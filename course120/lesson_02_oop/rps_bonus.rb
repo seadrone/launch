df5f2f59d464df44b18bf4fab83e1bb576c944ad
Grace D <gsdrone@gmail.com>
2016-04-25 03:54:25 +0000

    AI, personalities and scoreboard fix

--- course120/lesson_02_oop/rps_bonus.rb
+++ course120/lesson_02_oop/rps_bonus.rb
@@ -5,32 +5,29 @@ class Scoreboard
   def initialize(player1, player2)
     @p1 = player1
     @p2 = player2
-    initialize_board
-  end
-
-  def initialize_board
-    @board = {p1_history: ["#{p1.name}".slice(0, 10).center(10, ' ')],
-      divider: ['-' * 10],
-      p2_history: ["#{p2.name}".center(10, ' ')]
-    }
+    @board = {header: ["#{p1.name}".slice(0, 14), "#{p2.name}"]}
   end
 
   def update_board
-    @board[:p1_history].push('|').push("#{p1.move}".slice(0, 2).center(4, ' '))
-    @board[:divider].push('+').push('-' * 4)
-    @board[:p2_history].push('|').push("#{p2.move}".slice(0, 2).center(4, ' '))
+    rounds = @board.size - 1
+    @board[rounds+1] = [p1.move, p2.move]
   end
 
   def print_board
     puts ""
-    puts ""
-    puts "** SCOREBOARD **"
-    puts ""
-    puts @board[:p1_history].push('|').push("#{p1.score}".center(4, ' ')).join
-    puts @board[:divider].push('+').push("-" * 4).join
-    puts @board[:p2_history].push('|').push("#{p2.score}".center(4, ' ')).join
-    @board.each_value {|arr| arr.slice!(-2, 2)}
-    puts ""
+    puts "-" * 28
+    puts "SCOREBOARD".center(28, ' ')
+    puts "-" * 28
+    puts "#{board[:header][0]}".center(14, ' ') + '|' + "#{board[:header][1]}".center(14, ' ')
+    puts "-" * 14 + '+' + "-" * 14
+    i = 1
+    loop do
+      puts "#{board[i][0]}".center(14, ' ') + '|' + "#{board[i][1]}".center(14, ' ')
+      i += 1
+      break if i == @board.size
+    end
+    puts "-" * 28
+    puts "#{p1.score}".center(14, ' ') + '|' + "#{p2.score}".center(14, ' ')
     puts ""
   end
 
@@ -39,29 +36,33 @@ end
 class Player
   attr_accessor :move, :name, :score, :history
   def initialize
-    set_name
+    @history = []
     @score = 0
   end
 
   def set_move_choice(choice)
+    self.move = get_object(choice)
+  end
+
+  def get_object(choice)
     case choice
     when 'rock'
-      self.move = Rock.new
+      Rock.new
     when 'paper'
-      self.move = Paper.new
+      Paper.new
     when 'scissors'
-      self.move = Scissors.new
+      Scissors.new
     when 'lizard'
-      self.move = Lizard.new
+      Lizard.new
     when 'spock'
-      self.move = Spock.new
+      Spock.new
     end
   end
 end
 
 class Move
   VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-  attr_reader :defeats
+  attr_reader :defeats, :loses_to
 
   def initialize(value)
     @value = value
@@ -94,13 +95,13 @@ class Move
   def to_s
     @value
   end
-
 end
 
 class Rock < Move
   def initialize
     @value = 'rock'
     @defeats = ['scissors', 'lizard']
+    @loses_to = ['paper', 'spock']
   end
 end
 
@@ -108,6 +109,7 @@ class Paper < Move
   def initialize
     @value = 'paper'
     @defeats = ['rock', 'spock']
+    @loses_to = ['scissors', 'lizard']
   end
 end
 
@@ -115,6 +117,7 @@ class Scissors < Move
   def initialize
     @value = 'scissors'
     @defeats = ['paper', 'lizard']
+    @loses_to = ['rock', 'spock']
   end
 end
 
@@ -122,17 +125,22 @@ class Lizard < Move
   def initialize
     @value = 'lizard'
     @defeats = ['paper', 'spock']
+    @loses_to = ['scissors', 'rock']
   end
 end
 
 class Spock < Move
   def initialize
     @value = 'spock'
-    @defeats = ['scissorss', 'rock']
+    @defeats = ['scissors', 'rock']
+    @loses_to = ['lizard', 'paper']
   end
 end
 
 class Human < Player
+  def initialize
+    set_name
+  end
   def set_name
     n = ''
     system 'clear'
@@ -154,16 +162,86 @@ class Human < Player
       puts "Sorry, that's not a valid choice."
     end
     set_move_choice(choice)
+    self.history << self.move
   end
 end
 
 class Computer < Player
-  def set_name
-    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number5'].sample
+  def choose(opponent)
+    if opponent.history.length < 2
+      self.move = set_move_choice(Move::VALUES.sample)
+      self.history << self.move
+    else
+      occurrences = {}
+      opponent.history.each do |move|
+        if occurrences[move.to_s]
+          occurrences[move.to_s] += 1
+        else 
+          occurrences[move.to_s] = 1
+        end
+      end
+      max = occurrences.values.sort {|a,b| b <=> a}.first
+      smart_sample = []
+      highest_occurring_moves = occurrences.select {|k,v| v >= max}.keys
+      highest_occurring_moves.each do |move|
+        get_object(move).loses_to.map {|str| smart_sample << str}
+      end
+      self.move = set_move_choice(smart_sample.sample)
+      self.history << self.move
+    end
   end
 
-  def choose
-    self.move = set_move_choice(Move::VALUES.sample)
+  def name
+    @name.capitalize
+  end
+end
+
+class R2D2 < Computer # will not choose a Star Trek character
+  def initialize
+    @name = 'r2d2'
+  end
+  def choose(opponent)
+    self.move = set_move_choice(['rock', 'paper', 'scissors', 'lizard'].sample)
+    self.history << self.move
+  end
+  def name
+    "R2D2"
+  end
+end
+
+class Hal < Computer # only chooses from moves that can kill humans
+  def initialize
+    @name = 'hal'
+  end
+  def choose(opponent)
+    self.move = set_move_choice(['rock', 'scissors'].sample)
+    self.history << self.move
+  end
+end
+
+class Chappie < Computer # default bot behavior
+  def initialize
+    @name = 'chappie'
+  end
+end
+
+class Sonny < Computer # only chooses moves that start with 's'
+  def initialize
+    @name = 'sonny'
+  end
+  def choose(opponent)
+    self.move = set_move_choice(['scissors', 'spock'].sample)
+    self.history << self.move
+  end
+end
+
+class Number5 < Computer # only chooses moves that have more than 5 letters
+  def initialize
+    @name = 'number5'
+  end
+  def choose(opponent)
+    self.move = set_move_choice(['scissors', 'lizard'].sample)
+    self.history << self.move
   end
 end
 
@@ -176,13 +254,25 @@ class RPSGame
     @computer = Computer.new
   end
 
+  def titlelize(arr)
+    i = 0
+    title = ""
+    loop do
+      title << arr[i].capitalize << ', '
+      i += 1
+      break if i == arr.length
+    end
+    title.chop.chop
+  end
+
   def display_welcome_message
-    puts "Hi #{human.name}! Welcome to Rock, Paper, Scissors, Lizard, Spock!"
+    # puts "Hi #{human.name}! Welcome to Rock, Paper, Scissors, Lizard, Spock!"
+    puts "Hi, #{human.name}! Welcome to #{titlelize(Move::VALUES)}!"
     puts "The first player to win #{GAMES_TO_WIN} games wins. Good luck."
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Goodbye!"
+    puts "Thanks for playing #{titlelize(Move::VALUES)}. Goodbye!"
   end
 
   def display_moves
@@ -211,10 +301,10 @@ class RPSGame
     end
   end
 
-  def play_again?
+  def play_again?(str)
     answer = nil
     loop do
-      puts "Play another round? (y/n)"
+      puts "#{str}"
       answer = gets.chomp
       break if ['y', 'n'].include? answer.downcase
       puts "Please enter y or n."
@@ -232,46 +322,72 @@ class RPSGame
     puts "#{overall_winner.name} won the game!"
   end
 
-  def overall_play_again?
-    answer = nil
-    loop do
-      puts "Would you like to play another game? (y/n)"
-      answer = gets.chomp
-      break if ['y', 'n'].include? answer.downcase
-      puts "Please enter y or n."
-    end
-    return false if answer.downcase == 'n'
-    return true if answer.downcase == 'y'
-  end
-
   def update_score
     human.score += 1 if calculate_winner == human.name
     computer.score += 1 if calculate_winner == computer.name
   end
 
-  # def display_score
-    # puts "#{human.name}: #{human.score}, #{computer.name}: #{computer.score}"
-  # end
+  def choose_bot
+    choice = nil
+    loop do
+      puts ""
+      puts "Please choose an opponent: R2D2, Hal, Chappie, Sonny or Number5."
+      puts "Feel free to type just the first letter of the bot's name."
+      choice = gets.chomp.downcase
+      break if (['r2d2', 'hal', 'chappie', 'sonny', 'number5'].include? choice) ||
+        (['r', 'h', 'c', 's', 'n'].include? choice)
+      puts "Sorry, that's not a valid choice."
+    end
+    @computer = get_bot(choice)
+  end
+
+  def get_bot(choice)
+    case choice
+    when 'hal'
+      Hal.new 
+    when 'h'
+      Hal.new
+    when 'r2d2'
+      R2D2.new
+    when 'r'
+      R2D2.new
+    when 'chappie'
+      Chappie.new
+    when 'c'
+      Chappie.new
+    when 'sonny'
+      Sonny.new
+    when 's'
+      Sonny.new
+    when 'number5'
+      Number5.new
+    when 'n'
+      Number5.new
+    end
+  end
 
   def play
     loop do
       display_welcome_message
+      choose_bot
       human.score = 0
       computer.score = 0
+      human.history = []
+      computer.history = []
       board = Scoreboard.new(human, computer)
       loop do
+        computer.choose(human)
         human.choose
-        computer.choose
         display_moves
         calculate_winner
         display_winner
         update_score
         board.update_board
         board.print_board
-        break if overall_winner? || !play_again?
+        break if overall_winner? || !play_again?("Play another round?")
       end
       display_overall_winner if overall_winner?
-      break unless overall_play_again?
+      break unless play_again?("Would you like to play another #{GAMES_TO_WIN}-round game?")
     end
     display_goodbye_message
   end
