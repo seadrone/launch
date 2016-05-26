d5b8191fbd0a0805419c00e75de205539ac4b86b
Grace D <gsdrone@gmail.com>
2016-05-26 04:42:09 +0000

    seat approach

--- course130/lesson02_testing/car_test.rb
+++ course130/lesson02_testing/car_test.rb
@@ -4,17 +4,18 @@ Minitest::Reporters.use!
 require_relative 'car'
 
 class CarTest < MiniTest::Test
-  # def test_wheels
-  #   skip
-  #   car = Car.new
-  #   assert_equal(4, car.wheels)
-  # end
+  def setup
+    @car = Car.new
+  end
   
-  # def test_bad_wheels
-  #   skip
-  #   car = Car.new
-  #   assert_equal(3, car.wheels)
-  # end
+  def test_wheels
+    assert_equal(4, @car.wheels)
+  end
+  
+  def test_bad_wheels
+    skip
+    assert_equal(3, @car.wheels)
+  end
   
   # describe 'Car#wheels' do
   #   it 'has 4 wheels' do
@@ -23,32 +24,28 @@ class CarTest < MiniTest::Test
   #   end
   # end
   
-  # def test_car_exists
-  #   car = Car.new
-  #   assert(car)
-  # end
+  def test_car_exists
+    assert(@car)
+  end
   
-  # def test_name_is_nil
-  #   car = Car.new
-  #   # car.name = "Bessy"
-  #   assert_nil(car.name)
-  # end
+  def test_name_is_nil
+    assert_nil(@car.name)
+  end
   
-  # def test_raise_initialize_with_arg
-  #   assert_raises(ArgumentError) do
-  #     car = Car.new(name: "Joey")
-  #   end
-  # end
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")
+    end
+  end
   
-  # def test_instance_of_car
-  #   car = Car.new
-  #   assert_instance_of(Car, car)
-  # end
+  def test_instance_of_car
+    car = Car.new
+    assert_instance_of(Car, @car)
+  end
   
   def test_includes_car
-    car = Car.new
     arr = [1, 2, 3]
-    arr << car
-    assert_includes(arr, car)
+    arr << @car
+    assert_includes(arr, @car)
   end
 end
